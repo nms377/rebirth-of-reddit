f246ba57f159ca2a137fa0a4d81564900c215f6d
nms377 <nms377@nyu.edu>
2016-12-17 20:12:44 -1000

    Trying to figure out how to get my images to load.

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,16 @@ let source = JSON.parse(this.responseText);
 console.log(source);
 
 	for (var i = 0; i < source.data.children.length; i++) {
-		
+
 	//create child box for each post
 	let container = document.createElement('div');
 	container.id = 'container';
 	contentContainer.appendChild(container);
+
+	//gets image from post
+	let image = document.createElement('img');
+	image.setAttribute('src', 'source.data.children[i].data.url');
+
 
 	//gets title of post
 	let title = document.createElement('h2'); 
@@ -23,13 +28,14 @@ console.log(source);
 	//gets author of post
 	let author = document.createElement('p');
 	author.innerHTML = source.data.children[i].data.author;
-
+	container.appendChild(image);
 	container.appendChild(title);
 	container.appendChild(author);
 	}
+
 }
 
-var oReq = new XMLHttpRequest();
+const oReq = new XMLHttpRequest();
 oReq.addEventListener('load', reqListener);
 oReq.open('GET', 'https://www.reddit.com/r/aww/.json');
 oReq.send();
